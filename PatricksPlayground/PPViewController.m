6e3134c12f5d1449c2f4da98ddad6cd1601044ad
Patrick Magen <paddysnetworks@hotmail.de>
2013-07-04 15:36:14 +0200

    UI tweaks & gitignore

--- PatricksPlayground/PPViewController.m
+++ PatricksPlayground/PPViewController.m
@@ -7,16 +7,20 @@
 //
 
 #import "PPViewController.h"
-#import "PPColorRegulator.h"
 
 @interface PPViewController ()
 
 - (void)buttonPressed:(UIButton*)sender;
 - (void)sliderValueChanged:(UISlider*)sender;
  
-@property (strong, nonatomic) PPColorRegulator *colorRegulator;
 @property (nonatomic, copy) NSString *Label;
 @property (nonatomic) float *value;
+
+@property (strong, nonatomic) UISlider *sliderRed;
+@property (strong, nonatomic) UISlider *sliderGreen;
+@property (strong, nonatomic) UISlider *sliderBlue;
+
+@property (strong, nonatomic) UIColor *sliderLabelTextColor;
 
 @end
 
@@ -24,76 +28,90 @@
 
 - (void)viewDidAppear:(BOOL)animated
 {
-    self.colorRegulator = [[PPColorRegulator alloc] init];
-    
     UIButton *button1 = [UIButton buttonWithType:UIButtonTypeRoundedRect];
     
-    CGRect rectForButton1 = CGRectMake(60, 205, 200, 50);
+    CGRect rectForButton1 = CGRectMake(75, 400, 170, 40);
     
     [button1 addTarget:self action:@selector(buttonPressed:) forControlEvents:UIControlEventTouchDown];
     
     button1.frame = rectForButton1;
     
     [button1 setTitle:@"RESET" forState:UIControlStateNormal];
+    [button1 setTitle:@"nigger works" forState:UIControlStateHighlighted];
+
+    
+    button1.titleLabel.font = [UIFont fontWithName:@"AvenirNext-UltraLight" size:28];
     
     [self.view addSubview:button1];
     
     
-    UISlider *slider1 = [[UISlider alloc] init];
-    UISlider *slider2 = [[UISlider alloc] init];
-    UISlider *slider3 = [[UISlider alloc] init];
+    self.sliderRed = [[UISlider alloc] init];
+    self.sliderGreen = [[UISlider alloc] init];
+    self.sliderBlue = [[UISlider alloc] init];
     
-    UILabel *sliderLabel1 = [[UILabel alloc] init];
-    UILabel *sliderLabel2 = [[UILabel alloc] init];
-    UILabel *sliderLabel3 = [[UILabel alloc] init];
+    UILabel *sliderLabelRed = [[UILabel alloc] init];
+    UILabel *sliderLabelGreen = [[UILabel alloc] init];
+    UILabel *sliderLabelBlue = [[UILabel alloc] init];
     
-    CGRect rectForSlider1 = CGRectMake(15,  15, 90, 40);
-    CGRect rectForSlider2 = CGRectMake(115, 15, 90, 40);
-    CGRect rectForSlider3 = CGRectMake(215, 15, 90, 40);
+    CGRect rectForSliderRed = CGRectMake(15,  15, 90, 40);
+    CGRect rectForSliderGreen = CGRectMake(115, 15, 90, 40);
+    CGRect rectForSliderBlue = CGRectMake(215, 15, 90, 40);
     
-    CGRect rectForsliderLabel1 = CGRectMake(20, 60, 60, 30);
-    CGRect rectForsliderLabel2 = CGRectMake(120, 60, 60, 30);
-    CGRect rectForsliderLabel3 = CGRectMake(220, 60, 60, 30);
+    CGRect rectForsliderLabelRed = CGRectMake(20, 60, 60, 40);
+    CGRect rectForsliderLabelGreen = CGRectMake(120, 60, 60, 40);
+    CGRect rectForsliderLabelBlue = CGRectMake(220, 60, 60, 40);
     
     
-    [slider1 addTarget:self action:@selector(sliderValueChanged:) forControlEvents:UIControlEventValueChanged];
-    [slider2 addTarget:self action:@selector(sliderValueChanged:) forControlEvents:UIControlEventValueChanged];
-    [slider3 addTarget:self action:@selector(sliderValueChanged:) forControlEvents:UIControlEventValueChanged];
+    [self.sliderRed addTarget:self action:@selector(sliderValueChanged:) forControlEvents:UIControlEventValueChanged];
+    [self.sliderGreen addTarget:self action:@selector(sliderValueChanged:) forControlEvents:UIControlEventValueChanged];
+    [self.sliderBlue addTarget:self action:@selector(sliderValueChanged:) forControlEvents:UIControlEventValueChanged];
     
-    slider1.frame = rectForSlider1;
-    slider2.frame = rectForSlider2;
-    slider3.frame = rectForSlider3;
+    self.sliderRed.frame = rectForSliderRed;
+    self.sliderGreen.frame = rectForSliderGreen;
+    self.sliderBlue.frame = rectForSliderBlue;
     
-    sliderLabel1.frame = rectForsliderLabel1;
-    sliderLabel2.frame = rectForsliderLabel2;
-    sliderLabel3.frame = rectForsliderLabel3;
+    sliderLabelRed.frame = rectForsliderLabelRed;
+    sliderLabelGreen.frame = rectForsliderLabelGreen;
+    sliderLabelBlue.frame = rectForsliderLabelBlue;
 
-    [sliderLabel1 setText:@"red"];
-    [sliderLabel2 setText:@"green"];
-    [sliderLabel3 setText:@"blue"];
+    [sliderLabelRed setText:@"red"];
+    [sliderLabelGreen setText:@"green"];
+    [sliderLabelBlue setText:@"blue"];
     
-    [self.view addSubview:slider1];
-    [self.view addSubview:slider2];
-    [self.view addSubview:slider3];
+    [sliderLabelRed setBackgroundColor:[UIColor clearColor]];
+    [sliderLabelGreen setBackgroundColor:[UIColor clearColor]];
+    [sliderLabelBlue setBackgroundColor:[UIColor clearColor]];
     
-    [self.view addSubview:sliderLabel1];
-    [self.view addSubview:sliderLabel2];
-    [self.view addSubview:sliderLabel3];
+    self.sliderLabelTextColor = [UIColor whiteColor];
+    
+
+    sliderLabelRed.font = [UIFont fontWithName:@"AvenirNext-UltraLight" size:22];
+    sliderLabelGreen.font = [UIFont fontWithName:@"AvenirNext-UltraLight" size:22];
+    sliderLabelBlue.font = [UIFont fontWithName:@"AvenirNext-UltraLight" size:22];
+
+    [self.view addSubview:self.sliderRed];
+    [self.view addSubview:self.sliderGreen];
+    [self.view addSubview:self.sliderBlue];
+    
+    [self.view addSubview:sliderLabelRed];
+    [self.view addSubview:sliderLabelGreen];
+    [self.view addSubview:sliderLabelBlue];
 }
 
 -(void)buttonPressed:(UIButton *)sender
 {
     NSLog(@"reset");
     
-    if ([sender.titleLabel.text isEqualToString:@"RESET"]) {
-        self.view.backgroundColor = [self.colorRegulator resetColor];
-    }
-}
+    self.sliderRed.value = 0;
+    self.sliderGreen.value = 0;
+    self.sliderBlue.value = 0;
+    self.view.backgroundColor = [UIColor colorWithRed:self.sliderRed.value green:self.sliderGreen.value blue:self.sliderBlue.value alpha:1];}
 
 - (void)sliderValueChanged:(UISlider *)sender
 {
     NSLog(@"%s", __PRETTY_FUNCTION__);
     
+    self.view.backgroundColor = [UIColor colorWithRed:self.sliderRed.value green:self.sliderGreen.value blue:self.sliderBlue.value alpha:1];
 }
 
 @end
